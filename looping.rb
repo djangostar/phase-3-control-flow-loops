613737dd40cc2478fa01c98b4cb2b584a3946a51
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-13 10:29:38 -0700

    commit tree e976afb9ec5fa8b15b55c3e3dcdd043519640a6d

--- looping.rb
+++ looping.rb
@@ -1,9 +1,8 @@
 def happy_new_year
-  # your code here
-  counter = 10
-  until counter == 0
-    puts counter
-    counter -= 1
+  i = 10
+  until i == 0
+    puts i
+    i -= 1
   end
   puts "Happy New Year!"
 end
@@ -22,17 +21,11 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
-  (1..100).each do |i|
-    puts fizzbuzz(i)
+  (1..100).each do |num|
+    puts fizzbuzz(num)
   end
 end
 
 def reverse_string(str)
-  # your code here
-  reversed_str = ""
-  str.length.times do |i|
-    reversed_str = str[i] + reversed_str
-  end
-  reversed_str
+  str.chars.reduce { |x, y| y + x }
 end
